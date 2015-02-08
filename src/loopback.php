8e0072d72e008c1f34afa133fc0c49278373db03
michealwillard <willardm@onid.oregonstate.edu>
2015-02-07 20:20:48 -0800

    Cleaned up multtable.php, made test_loopback.html, started loopback.php, updated log.txt, pushed changes.

---
+++ src/loopback.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<!--
+Micheal Willard
+Oregon State University
+CS 290-400
+Winter 2015
+Assignment 4 Part 1
+-->
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <title>Loopback</title>
+</head>
+<body>
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors',1);
+
+// If GET Input
+if($_SERVER['REQUEST_METHOD'] === 'GET'){
+
+}
+
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+}
+
+?>
+  </body>
+</html>
--- src/multtable.php
+++ src/multtable.php
@@ -17,10 +17,10 @@ error_reporting(E_ALL);
 ini_set('display_errors',1);
 
 // Function to get, check and set variables
+// Variables passed by reference
 function setVariable(&$variable, &$name){
   if(isset($_GET[$name])){
     if(null !== ($_GET[$name] = filter_input(INPUT_GET, $name, FILTER_VALIDATE_INT, FILTER_NULL_ON_FAILURE))){
-      // echo "setting variable";
       $variable = (int)$_GET[$name];
       // echo $variable;
     }
@@ -35,6 +35,8 @@ function setVariable(&$variable, &$name){
   }
 }
 
+// Function to build the tables
+// Variables passed by reference
 function buildTable(&$a, &$b, &$c, &$d){
   $columns = $d - $c + 2; //width
   $rows = $b - $a + 2; //height
@@ -46,21 +48,20 @@ function buildTable(&$a, &$b, &$c, &$d){
     echo '<th>' . ($c + $i) . '</th>';
   }
   echo '</tr>';
-  // build the remaining rows
+  // Build the Remaining Rows
   for($j = 0; $j < $rows - 1; $j++){
     for($k = 0; $k < $columns; $k++){
       if($k == 0){
-        // for first item in row, min-multiplicand + row index $j
+        // Build Header for 1st item in row, min-multiplicand + row index $j
         echo '<th>' . ($a + $j) . '</th>';
       }
       else {
-        //else min-multiplicand + row index * min-multiplier + col index $k
+        //else min-multiplicand + row index * min-multiplier + col index $k - 1
         echo '<td>' . (($a + $j) * ($c + $k - 1)) . '</td>';
       }
     }
     echo '</tr>';
   }
-  // echo "</tr>"
   echo "</table>";
 }
 
@@ -75,11 +76,13 @@ $minMultiplier_name = 'min-multiplier';
 $maxMultiplier = null;
 $maxMultiplier_name = 'max-multiplier';
 
+// Call Functions to get Variable Values
 setVariable($minMultiplicand, $minMultiplicand_name);
 setVariable($maxMultiplicand, $maxMultiplicand_name);
 setVariable($minMultiplier, $minMultiplier_name);
 setVariable($maxMultiplier, $maxMultiplier_name);
 
+// Check the Min and Maxs Values agree
 if($minMultiplicand > $maxMultiplicand) {
   echo "<p>ERROR: Minimum Multiplicand is greater than Maximum.";
 }
@@ -87,25 +90,9 @@ elseif($minMultiplier > $maxMultiplier) {
   echo "<p>ERROR: Minimum Multiplier is greater than Maximum.";
 }
 else {
-  // echo "<p>multiplicands and multipliers are ok.";
   buildTable($minMultiplicand, $maxMultiplicand, $minMultiplier, $maxMultiplier);
 }
-
-// if(isset($minMultiplicand)) {
-//   echo "<p>min-multiplicand is: " . $minMultiplicand;
-// }
-// if(isset($maxMultiplicand)) {
-//   echo "<p>min-multiplicand is: " . $maxMultiplicand;
-// }
-// if(isset($minMultiplier)) {
-//   echo "<p>min-multiplicand is: " . $minMultiplier;
-// }
-// if(isset($maxMultiplier)) {
-//   echo "<p>min-multiplicand is: " . $maxMultiplier;
-// }
-
-//?min-multiplicand=4&max-multiplicand=4&min-multiplier=4&max-multiplier=4
-
+//?min-multiplicand=1&max-multiplicand=12&min-multiplier=1&max-multiplier=12
 ?>
   </body>
 </html>

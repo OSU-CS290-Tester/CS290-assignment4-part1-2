9777a5b0d9211b26e68b97939af3bba8ba254ef8
michealwillard <willardm@onid.oregonstate.edu>
2015-02-08 10:20:35 -0800

    Completed loopback.php, finished testing, updated log and pushed.

--- src/loopback.php
+++ src/loopback.php
@@ -16,15 +16,45 @@ Assignment 4 Part 1
 error_reporting(E_ALL);
 ini_set('display_errors',1);
 
-// If GET Input
-if($_SERVER['REQUEST_METHOD'] === 'GET'){
-
+// Function to Return Input
+function getInput(&$type){
+  if($_SERVER['REQUEST_METHOD'] === 'GET'){
+    // echo 'GET';
+    $type = 'GET';
+    return $_GET;
+  }
+  elseif($_SERVER['REQUEST_METHOD'] === 'POST'){
+    // echo 'POST';
+    $type = 'GET';
+    return $_POST;
+  }
 }
 
-if($_SERVER['REQUEST_METHOD'] === 'POST'){
-
+// Function to build the object
+function buildObject(&$type, &$input){
+  if($input === null){
+    // No Parameters (key/value pairs) Passed in
+    $input = array('Type' => $type, 'parameters' => 'null');
+    return $input;
+  }
+  else{
+    // Check for null parameters
+    foreach($input as $key => $value){
+      if($value === null){
+        $input[$key] = null;
+      }
+    }
+    $input = array('Type' => $type, 'parameters' => $input);
+    return $input;
+  }
 }
 
+//***** Main Code Section *****
+$inputType = null;
+$inputParams = getInput($inputType);
+$inputParams = buildObject($inputType, $inputParams);
+$jsonObj = json_encode($inputParams);
+echo $jsonObj . "<br>";
 ?>
   </body>
 </html>

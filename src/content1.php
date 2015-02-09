a0338bca39cad6113e311eef289a36fcc73c834a
michealwillard <willardm@onid.oregonstate.edu>
2015-02-08 17:40:13 -0800

    Got login.php and content1.php almost finished. Undefined index errors on login ln 14,24 content1 ln16.

---
+++ src/content1.php
@@ -0,0 +1,86 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors',1);
+session_start();
+
+//***** File Path Variables *****
+$filePath = explode('/', $_SERVER['PHP_SELF'], -1);
+$filePath = implode('/', $filePath);
+$redirect = "http://" . $_SERVER['HTTP_HOST'] . $filePath;
+$redirectLogin = $redirect . '/login.php';
+$redirectLogout = $redirectLogin . '?loggedOut=true';
+$redirectContent2 = $redirect . '/content2.php';
+
+//***** Text Display Variables *****
+$displayText = "";
+$sessionActiveText = "<p>Hi $_SESSION[name], you have visited this page $_SESSION[visits] times.<p>Click <a href=\"$redirectLogout\">here</a> to logout.<p>Click <a href=\"$redirectContent2\">here</a> to visit Content 2.";
+$sessionNotActiveText = "<p>ERROR: You didn't enter a Username..
+<p>Please click <a href=\"$redirectLogout\">here</a> to return to the login page.";
+
+//***** Main Body Code *****
+// Approach adapted from O'Reilly PHP and MySQL Ch. 10-11.
+// Check for user name on first visit.
+if(isset($_POST['username'])){
+  // If username is empty redirect to login.
+  if($_POST['username'] === "" || $_POST['username'] === null){
+    $displayText =  $sessionNotActiveText;
+    $_SESSION['sessionActive'] = false;
+  }
+  //Else a Username was entered.
+  else{
+    // Confirm $_SESSION[username] not set
+    if(!isset($_SESSION['name'])){
+      $_SESSION['name'] = $_POST['username'];
+      $_SESSION['sessionActive'] = true;
+      $_SESSION['visits'] = 0;
+      $displayText =  $sessionActiveText;
+      $_SESSION['visits']++;
+    }
+  }
+}
+if(!isset($_POST['username'])){
+  $displayText =  $sessionNotActiveText;
+  $_SESSION['sessionActive'] = false;
+}
+// Check if user is already logged in.
+if(isset($_SESSION['name'])){
+  if(isset($_POST['username'])){
+    if($_SESSION['name'] != $_POST['username']){
+      $_SESSION['name'] = $_POST['username'];
+      $_SESSION['sessionActive'] = 'true';
+      $_SESSION['visits'] = 0;
+      $displayText =  $sessionActiveText;
+      $_SESSION['visits']++;
+    }
+    else{
+      $displayText =  $sessionActiveText;
+      $_SESSION['visits']++;
+    }
+  }
+  else{
+    $displayText =  $sessionActiveText;
+    $_SESSION['visits']++;
+  }
+}
+// Redirect final condition for sessionActive not set
+if(!isset($_SESSION['sessionActive'])){
+  header("Location: {$redirect}/login.php");
+}
+?>
+<!DOCTYPE html>
+<!--
+Micheal Willard
+Oregon State University
+CS 290-400
+Winter 2015
+Assignment 4 Part 1
+-->
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <title>Content 1 Page</title>
+</head>
+<body>
+  <?php echo $displayText; ?>
+</body
+</html>
---
+++ src/login.php
@@ -0,0 +1,54 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors',1);
+session_start();
+$filePath = explode('/', $_SERVER['PHP_SELF'], -1);
+$filePath = implode('/', $filePath);
+$redirect = "http://" . $_SERVER['HTTP_HOST'] . $filePath;
+$redirectLogin = $redirect . '/login.php';
+$redirectLogout = $redirectLogin . '?loggedOut=true';
+$redirectContent1 = $redirect . '/content1.php';
+$redirectContent2 = $redirect . '/content2.php';
+
+$displayText = "";
+$sessionActiveText = "<p>Hi $_SESSION[name].<p>Click <a href=\"$redirectContent1\">here</a> to visit Content 1.<p> If this is not you, click <a href=\"$redirectLogout\">here</a> to logout.";
+
+//checks if 'logged' exists in URL
+//and logged is true, i.e. user is logged off
+if(isset($_GET['loggedOut']) && $_GET['loggedOut'] === 'true'){
+  //destroy session code
+  $_SESSION = array(); //empty session data array
+  session_destroy(); //destroy session
+  $displayText = "<p>You have succesfully logged out.";
+}
+if($_SESSION['sessionActive'] === 'true'){
+  $displayText = $sessionActiveText;
+}
+?>
+<!DOCTYPE html>
+<!--
+Micheal Willard
+Oregon State University
+CS 290-400
+Winter 2015
+Assignment 4 Part 1
+-->
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <title>Login Page</title>
+</head>
+<body>
+  <php? echo $displayText; ?>
+  <form action=<?=$redirectContent1?> method="post">
+    <fieldset>
+      <label>Username:</label>
+      <input type="text" name="username" size="30" maxlength="100">
+    </fieldset>
+    <br>
+    <fieldset>
+      <input type="submit" value="Login">
+    </fieldset>
+  </form>
+</body>
+</html>
